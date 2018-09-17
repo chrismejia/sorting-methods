36305804246952b8b2faebd2e5affe66fc706497
Christian Mejia <chrismejia10@gmail.com>
2018-09-17 15:14:20 -0400

    Add plan of attach

--- mergesort.js
+++ mergesort.js
@@ -1,6 +1,24 @@
-//yoyoyoyoyoyoyoy
+// MERGE SORT
 
-// This is
-// an example
-// of me pushing
-// to you.
+// How to go about it
+// 1. We split the array repeatedly until we have sizes of 1
+// 2. Invoke merge()
+//
+
+// DIAGRAM
+// mergeSort(arr) {
+//    split(){
+//      recursively reduces size down to 1 entry each
+//      does this by splitting in half repeatedly
+//      BASE CASE
+//      -- let currNode to examine currNode
+//      -- if currNode.length === 1 {split(currNode)}
+//      RECURSIVE CASE
+//      -- if currNode.length > 1 {split(currNode)}
+//    }
+//
+//    merge() {
+//      takes each element and evaluates it against its neighbor
+//      builds
+//    }
+// }
