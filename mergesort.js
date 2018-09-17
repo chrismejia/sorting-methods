3906c21ca4f09ad7bd17c8abdadf9d33d2e03d08
Christian Mejia <chrismejia10@gmail.com>
2018-09-17 15:40:12 -0400

    Add working array half splits

--- mergesort.js
+++ mergesort.js
@@ -22,3 +22,33 @@
 //      builds
 //    }
 // }
+
+//--- HELPER FUNCTIONS ---
+function split(arr) {
+  // Arrays split with non-integer index values
+  // e.g. [1,2,3] => [1,2,3].slice(1.5) => [2,3]
+  // Initial divide point; will be redefined inside recursive cases
+  let dividePoint = arr.length / 2;
+
+  let firstHalf = arr.slice(0, dividePoint);
+  let secondHalf = arr.slice(dividePoint);
+
+  // STORING THE DIVIDED THINGS
+  let prelimSplitArr = [firstHalf, secondHalf];
+  // console.log(prelimSplitArr); ==> THESE NESTED ARRAYS F'IN WORK
+
+  // Loop over both 'halves' aka each of the two nested arrays to continually split and reassign firstHalf and secondHalf
+
+  // BASE CASE
+  // If the arr has length 1...
+  if (arr.length === 1) {
+    // ...you're done, find a way to return the whole shabang
+    //code
+  }
+  // arr.length > 1
+  else {
+    return split(firstHalf);
+  }
+  // return fully split array
+  return [firstHalf, secondHalf];
+}
